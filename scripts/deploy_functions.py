8cac371da67fcae18385dfa1d263029d4c2f2ba1
Hoang Tran <trhoang2003@gmail.com>
2025-06-10 22:10:48 -0700

    .

--- scripts/deploy_functions.py
+++ scripts/deploy_functions.py
@@ -227,7 +227,7 @@ def deploy_to_aws(workflow_data):
                 lambda_client.create_function(
                     FunctionName=actual_func_name,
                     PackageType='Image',
-                    Code={'ImageUri': '145342739029.dkr.ecr.us-east-1.amazonaws.com/aws-lambda-tidyverse:latest'},
+                    Code={'ImageUri': '992382457530.dkr.ecr.us-west-2.amazonaws.com/create_sample_data'},
                     Role=role_arn,
                     Timeout=300,
                     MemorySize=256
@@ -236,7 +236,7 @@ def deploy_to_aws(workflow_data):
                 # Update existing function
                 lambda_client.update_function_code(
                     FunctionName=actual_func_name,
-                    ImageUri='145342739029.dkr.ecr.us-east-1.amazonaws.com/aws-lambda-tidyverse:latest'
+                    ImageUri='992382457530.dkr.ecr.us-west-2.amazonaws.com/create_sample_data'
                 )
             
             print(f"Successfully deployed {actual_func_name} to AWS Lambda")
